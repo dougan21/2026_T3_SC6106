4513c79377c085006cfc21a777e0bd304cf00806
dougan21 <785985468@qq.com>
2026-03-19 18:52:04 +0800

    [update]

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return("<h2>Hi</h2>")
+    return(render_template('index.html'))
 
 if __name__ == '__main__':
     app.run()
